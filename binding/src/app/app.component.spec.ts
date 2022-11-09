46dfe093e65b5f0da3b9ff35230ee0a0e404ef50
pratikkumar2212 <kumarpratik2212@gmail.com>
2022-11-09 09:53:42 +0000

    first

--- binding/src/app/app.component.spec.ts
+++ binding/src/app/app.component.spec.ts
@@ -1,35 +1,31 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  beforeEach(() => {
+    component = new AppComponent();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('test_case1',()=>{
+      component.animalPosition = 0;
+      expect(component.animalSource[component.animalPosition]).toBe("https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg");
   });
-
-  it(`should have as title 'angularapp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularapp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angularapp app is running!');
-  });
+  it('test_case2',()=>{
+    component.animalPosition = 1;
+    expect(component.animalSource[component.animalPosition]).toBe("https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg");
+});
+it('test_case3',()=>{
+  component.animalPosition = 2;
+  expect(component.animalSource[component.animalPosition]).toBe("https://images.pexels.com/photos/1598377/pexels-photo-1598377.jpeg");
+});
+it('test_case4',()=>{
+  component.animalPosition = 3;
+  expect(component.animalSource[component.animalPosition]).toBe("https://images.pexels.com/photos/2541239/pexels-photo-2541239.jpeg");
+});
+it('test_case5',()=>{
+  component.animalPosition = 4;
+  expect(component.animalSource[component.animalPosition]).toBe("https://images.pexels.com/photos/3551498/pexels-photo-3551498.jpeg");
+});
 });
